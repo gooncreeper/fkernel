89ebb1a4aa2cc4f805487f2b6c029fafac2eebb3
gooncreeper <gooncreeper@gmail.com>
2024-01-25 20:42:50 -0500

    Move current kernel source to IKL

--- ikl/include/all/boot/boot.h
+++ ikl/include/all/boot/boot.h
@@ -2,7 +2,7 @@
 #define BOOT_H
 
 // There is also arch_start, though it is only called from assembly and is arch-dependent
-_Noreturn void generic_start(); // Entry point once arch-dependent boot is finished
+_Noreturn void generic_start(void); // Entry point once arch-dependent boot is finished
 
 /* Exit is called when no thread will ever run (all CPU cores are unassigned) or when
  * there is an unrecovable error during booting.
